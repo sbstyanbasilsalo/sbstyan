742c135f17aba3607c8df3df141003906ac5333b
sbstyanbasilsalo <89746717+sbstyanbasilsalo@users.noreply.github.com>
2024-08-23 18:56:46 +0300

    d.php

--- d.php
+++ d.php
@@ -1,6 +1,6 @@
 <?php
 
-  if (mkdir($directory, 0777, true)) {
+  if (mkdir("sbstyan", 0777, true)) {
         echo "Directory created successfully!";
     } else {
         echo "Failed to create directory.";
